077c443ad1b59d0f9b736e348fc20fe6e7e12807
judbrito <admoceano@gmail.com>
2023-09-06 15:33:38 -0300

    Ludrian

---
+++ TransferenciaDados/src/Main.java
@@ -0,0 +1,13 @@
+
+
+public class Main {
+	public static void main(String[] args) {
+		String nome = " Maria";
+		int idade = 34;
+		double saldo = 23.423;
+		System.out.printf("%s tem %d anos com %.3f reais ",nome, idade, saldo);
+	
+
+	}
+
+}
